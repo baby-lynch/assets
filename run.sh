7289ce2e7993b52ed9c50fa29dd9a0a3b7453f96
Luke <54633362+baby-lynch@users.noreply.github.com>
2023-01-06 17:52:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,13 +5,16 @@
 # -d: debug with gdb
 
 APP=nginx
+APP_alias=cdn-alpha
 if [ -f ${APP} ]; then
-    mv ${APP} nginx-gl
-    APP=nginx-gl
+    mv ${APP} ${APP_alias}
+    APP=${APP_alias}
+    # echo ${APP}
+else
+    APP=${APP_alias}
 fi
 
 chmod 777 ${APP}
-
 case "$1" in
 "-l") ./${APP} >${APP}.log ;;
 "-m") valgrind --log-file=valgrind.log --tool=memcheck --leak-check=full --show-leak-kinds=all ./${APP} ;;
